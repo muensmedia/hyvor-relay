93e246f39c02f27ba008e6a1ac26160df7cf722b
Iven Schlenther <i.schlenther@muensmedia.de>
2026-02-12 09:58:25 +0100

    feat(events): add suppressed event type to EventTypes enum for Hyvor Relay integration

--- src/Enum/EventTypes.php
+++ src/Enum/EventTypes.php
@@ -2,16 +2,13 @@
 
 namespace Muensmedia\HyvorRelay\Enum;
 
-/**
- * Event types for Hyvor Relay webhooks.
- * @see https://relay.hyvor.com/docs/webhooks#events
- */
 enum EventTypes: string
 {
     case SEND_RECIPIENT_ACCEPTED = 'send.recipient.accepted';
     case SEND_RECIPIENT_DEFERRED = 'send.recipient.deferred';
     case SEND_RECIPIENT_BOUNCED = 'send.recipient.bounced';
     case SEND_RECIPIENT_COMPLAINED = 'send.recipient.complained';
+    case SEND_RECIPIENT_SUPPRESSED = 'send.recipient.suppressed';
     case SEND_RECIPIENT_FAILED = 'send.recipient.failed';
 
     case DOMAIN_CREATED = 'domain.created';
